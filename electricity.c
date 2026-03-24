b95292db35fe2d3daa8236bef2cf5f66fdff247a
gilbertmungufeni90-cell <gilbertmungufeni90@gmail.com>
2026-03-24 21:32:35 +0300

    Add files via upload

---
+++ electricity.c
@@ -0,0 +1,49 @@
+/* MUNGUFENI GILBERT */
+/* 25/U/BIO/01398 */
+
+#include <stdio.h>
+   int main(){
+
+          // declaring variables
+          float units_consumed, cost_per_unit, total_bill;
+
+
+        // assigning variables
+        printf("Enter the units consumed:");
+        scanf("%f", &units_consumed);
+
+        printf("Enter cost per unit:");
+        scanf("%f", &cost_per_unit);
+
+
+        //performing calculations
+        total_bill =( units_consumed * cost_per_unit);
+
+
+
+        //output of results
+        printf("\nThe total bill is: %.2f UGX\n", total_bill);
+
+
+
+
+
+
+
+   return 0;
+
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
